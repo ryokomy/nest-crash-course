2383df6085cba018414885982a44f227b21887f8
Ryo Komiyama <ryokomy@kyuzan.com>
2022-08-06 15:44:06 +0900

    Initial commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
+
+@ApiTags('users')
+@Controller('users')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @ApiOkResponse({ type: User, isArray: true })
+  @ApiQuery({ name: 'name', required: false })
+  @Get()
+  getUsers(@Query('name') name?: string): User[] {
+    return this.userService.findAll(name);
+  }
+
+  @ApiOkResponse({ type: User, description: 'the user' })
+  @ApiNotFoundResponse()
+  @Get(':id')
+  getUserById(@Param('id', ParseIntPipe) id: number): User {
+    // TODO: auto parse ID
+
+    const user = this.userService.findByID(id);
+    if (!user) {
+      throw new NotFoundException();
+    }
+    return user;
+  }
+
+  @ApiCreatedResponse({ type: User })
+  @ApiBadRequestResponse()
+  @Post()
+  createUser(@Body() body: CreateUserDto): User {
+    return this.userService.createUser(body);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  private users: User[] = [
+    { id: 0, name: 'Ryo' },
+    { id: 1, name: 'Komy' },
+    { id: 2, name: 'RyoKomy' },
+  ];
+
+  findAll(name?: string): User[] {
+    if (name) {
+      return this.users.filter((user) => user.name === name);
+    }
+    return this.users;
+  }
+
+  findByID(userId: number): User {
+    return this.users.find((user) => user.id === userId);
+  }
+
+  createUser(createUserDto: CreateUserDto): User {
+    const newUser = { id: Date.now(), ...createUserDto };
+    this.users.push(newUser);
+    return newUser;
+  }
+}
